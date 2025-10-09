21b5bfec343047b8fc844ac56dda21f47d105287
marceljogido <marcelmarbun33@gmail.com>
2025-10-09 12:18:21 +0700

    Support /videos route and ignore uploads

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/videos', videosRouter);
+app.use(['/api/videos', '/videos'], videosRouter);
 
 // Serve uploaded videos
 app.use('/uploads', express.static('uploads'));
